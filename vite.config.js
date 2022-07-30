ee88715944d763e42acfdbb0ee18d181969db53a
zmkwjx <zmkwjx@126.com>
2022-07-30 21:28:24 +0800

    feat: init

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import eslintPlugin from 'vite-plugin-eslint'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    // 增加下面的配置项,这样在运行时就能检查eslint规范
+    eslintPlugin({
+      include: ['src/**/*.js', 'src/**/*.vue', 'src/*.js', 'src/*.vue']
+    })
+  ]
+})
